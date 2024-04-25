7d063249453b5b800b3141cac2c90e82ab77cf11
Bautista Rosli <roslibautista05@gmail.com>
2024-04-25 19:09:33 -0300

    Primer commit, inciso 4 procesamiento y estructura

---
+++ modules/censo2022.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+import csv
+#se importan las librerías a utilizar por este módulo
+
+def create_modified_file(filename = str):
+    """
+    La funcion recibe como parámetro un string el cual utilizará como pauta para nombrar el archivo que va a crear
+    Lee el archivo 'c2022_tp_c_resumen_adaptado.csv', toma sus datos y los pasa al nuevo archivo generado 
+    agregando la columna 'Porcentaje de población en situación de calle' con sus respectivos datos numéricos.
+    """
+    #Se declaran las rutas de los archivos a utilizar
+    file_route = Path('datasets')/ 'c2022_tp_c_resumen_adaptado.csv'
+    new_file_route = Path('datasets_custom')/filename
+
+    #Se utiliza la sentencia "with" para manejar los archivos, aclarando el modo de abrirlos y su codificacion
+    with open(file_route,'r', encoding = 'UTF-8') as base, open(new_file_route,'w', encoding = 'UTF-8') as new:
+            reader_csv = csv.reader(base)
+            writer_csv = csv.writer(new)
+
+            header = []
+            header = next(reader_csv)
+            header.append('Porcentaje de población en situación de calle')
+            writer_csv.writerow(header) #Paso el header (primera línea) al nuevo archivo agregando el título de la nueva columna
+            
+            for line in reader_csv:
+                #Recorro todas las lineas del archivo viejo y las paso modificadas al nuevo
+                modified_line = list(map(lambda value : 0 if value in ['///','-'] else value, line))
+                #Se reemplazan los caracteres "///" y "-" que puedan existir dentro del dataset por el valor 0
+                percentage =  (int(modified_line[4]) / int(modified_line[1])) * 100 if int(modified_line[4]) != 0 else 0
+                #Se agrega el porcentaje de personas en situación de calle (datos de la nueva columna)
+                modified_line.append(percentage)
+                writer_csv.writerow(modified_line)
+                #Escribo los datos modificados en el nuevo archivo
