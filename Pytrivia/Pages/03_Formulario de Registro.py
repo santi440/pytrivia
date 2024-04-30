b26baab1b3d36d51277acebf81bb69bd4d514d9c
Federico Kesselman <kesselmanfederico@gmail.com>
2024-04-30 10:28:11 -0300

    Agrego comentarios

--- Pytrivia/Pages/03_Formulario de Registro.py
+++ Pytrivia/Pages/03_Formulario de Registro.py
@@ -11,10 +11,11 @@ def save_form_csv(username, full_name, email, birth_date, gender):
     ok = False
     create_header = False
 
+    # en variable ok se guarda si ya hay un usuario con ese mail
     with path.open(mode='r', encoding='UTF-8') as file:
         reader = csv.reader(file)
 
-        # chequea si csv esta vacio
+        # chequea si csv esta vacio para crear cabecera
         if path.stat().st_size == 0:
             create_header = True
 
@@ -22,7 +23,8 @@ def save_form_csv(username, full_name, email, birth_date, gender):
             if line[EMAIL] == email:
                 ok = True
                 break
-        
+    
+    # si ok es true se guardan todas las lineas en lines y se modifica la del usuario
     if ok:
         lines = []
         with path.open(mode='r', encoding='UTF-8') as file:
@@ -33,13 +35,16 @@ def save_form_csv(username, full_name, email, birth_date, gender):
                     line = [username, full_name, email, birth_date, gender]
                 lines.append(line)
 
+        # se escriben todas las lineas
         with path.open(mode='w', encoding='UTF-8', newline='') as file:
             writer = csv.writer(file)
             writer.writerows(lines)
     else:
+        # en caso de no estar se abre en modo A para agregar el usuario al final
         with path.open(mode='a', encoding='UTF-8', newline='') as file:
             writer = csv.writer(file)
 
+            # se crea cabecera si esta vacio el csv
             if create_header:
                 header = ['Usuario', 'Nombre completo', 'Email', 'Nacimiento','Genero']
                 writer.writerow(header)
