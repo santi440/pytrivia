e4c98916653396a21f66e61e386f318759d844f0
Ignacio <battaglinoignacio@gmail.com>
2024-05-02 16:35:25 -0300

    formulario de registro terminado

--- Pytrivia/Pages/03_Formulario de Registro.py
+++ Pytrivia/Pages/03_Formulario de Registro.py
@@ -4,31 +4,48 @@ import pathlib
 
 from datetime import date
 
+
 def save_form_csv(username, full_name, email, birth_date, gender):
     path = pathlib.Path('Csv/datos_formularios.csv')
     EMAIL = 2
+    ok = False
+    create_header = False
 
-    try:
-        with path.open(mode='r+', encoding='UTF-8', newline='') as file:
+    with path.open(mode='r', encoding='UTF-8') as file:
+        reader = csv.reader(file)
+
+        # chequea si csv esta vacio
+        if path.stat().st_size == 0:
+            create_header = True
+
+        for line in reader:
+            if line[EMAIL] == email:
+                ok = True
+                break
+        
+    if ok:
+        lines = []
+        with path.open(mode='r', encoding='UTF-8') as file:
             reader = csv.reader(file)
-            user = [username, full_name, email, birth_date, gender]
 
-            next (reader)
-            for row in reader:  # Ignora la primera fila que es la cabecera
-                if row[EMAIL] == email:
-                    row[:] = user  # Reemplaza todos los elementos de la fila con los nuevos datos
-                    break
-                else:
-                      
-                      # Agrega una nueva fila si el correo electrónico no existe
+            for line in reader:
+                if line[EMAIL] == email:
+                    line = [username, full_name, email, birth_date, gender]
+                lines.append(line)
 
-            # Escribe todo el contenido de nuevo al archivo
-            file.seek(0)  # Regresa al inicio del archivo
+        with path.open(mode='w', encoding='UTF-8', newline='') as file:
             writer = csv.writer(file)
-            writer.writerows(rows)
+            writer.writerows(lines)
+    else:
+        with path.open(mode='a', encoding='UTF-8', newline='') as file:
+            writer = csv.writer(file)
 
-    except FileNotFoundError:
-        st.error("No se encontró el archivo CSV.")
+            if create_header:
+                header = ['Usuario', 'Nombre completo', 'Email', 'Nacimiento','Genero']
+                writer.writerow(header)
+
+            writer.writerow([username, full_name, email, birth_date, gender])
+
 
 st.title("Formulario de Registro")
 
@@ -39,18 +56,20 @@ with st.form("my_form"):
     email = st.text_input("Mail")
     birth_date = st.date_input("Fecha de Nacimiento")
     gender = st.selectbox("Género", ["Masculino", "Femenino", "Otro"])
-
+    
     # Botón para enviar el formulario
     submitted = st.form_submit_button("Enviar")
 
     # Chequeo de que el usuario ingrese bien los datos
     if submitted:
         if not username or not full_name or not email:
-            st.error("Complete todos los campos para continuar.")
+            st.error ("Complete todos los campos para continuar.")
         elif not '@' in email or not '.com' in email:
-            st.error("Ingrese un email válido.")
+            st.error ("Ingrese un email válido.")
         elif birth_date >= date.today():
-            st.error("Ingrese una fecha válida.")
+            st.error ("Ingrese una fecha válida.")
         else:
             save_form_csv(username, full_name, email, birth_date, gender)
             st.success("Formulario enviado con éxito!")
+            
+        
