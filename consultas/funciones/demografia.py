113ecebb6ac1c198d3aaeb68a9ea75af086bba6f
Matias Santiago Marcos <santiago0105@hotmail.com.ar>
2024-05-02 09:59:23 -0300

    Update 2 files
    - /consultas/funciones/demografia.py
    - /consultas/incisos 4,5 y 6.ipynb

--- consultas/funciones/demografia.py
+++ consultas/funciones/demografia.py
@@ -19,21 +19,6 @@ def __pedir_datos_usuario():
         else:
             print("Por favor, ingrese un número entero")
             
-
-def __leer_archivo_csv(ruta):
-    """
-    Lee un archivo CSV y devuelve sus datos.
-
-    Args:
-        ruta (str): La ruta del archivo CSV a leer.
-
-    Returns:
-        list: Una lista que contiene los datos del archivo CSV, donde cada elemento es una fila del archivo.
-    """
-    with open(ruta, 'r', encoding="utf-8") as file:
-        reader = csv.reader(file)
-        return list(reader)
-
 def __filtrar_provincias_por_poblacion(ecuacion, datos_censo):
     """
     Filtra las provincias según la ecuación proporcionada y los datos del censo.
@@ -45,15 +30,17 @@ def __filtrar_provincias_por_poblacion(ecuacion, datos_censo):
     Returns:
         dict: Un diccionario que contiene las provincias que cumplen con la condición proporcionada.
     """
-    poblacion_filtrada = {}
-    for line in datos_censo:
-        if eval(ecuacion + line[1]):
-            #Guardo en el diccionario nombre de provincia y un subdiccionario con 3 listas, en principio vacias
-            poblacion_filtrada[line[0]] = {
-                "aeropuertos": [],
-                "lagos": [],
-                "tipos_conectividad": []
-            }
+    with open(ruta, 'r', encoding="utf-8") as file:
+        reader = csv.reader(file)
+        poblacion_filtrada = {}
+        for line in datos_censo:
+            if eval(ecuacion + line[1]):
+                #Guardo en el diccionario nombre de provincia y un subdiccionario con 3 listas, en principio vacias
+                poblacion_filtrada[line[0]] = {
+                                "aeropuertos": [],
+                                "lagos": [],
+                                "tipos_conectividad": []
+                                }
     return poblacion_filtrada
 
 def __agregar_lagos_a_provincias(datos_lagos, provincias):
@@ -64,12 +51,14 @@ def __agregar_lagos_a_provincias(datos_lagos, provincias):
         datos_lagos (list): Una lista que contiene los datos de los lagos.
         provincias (dict): Un diccionario que contiene las provincias a las que se agregarán los lagos.
     """
-    for line in datos_lagos:
-        ubicacion = line[1].split(" / ")
+    with open(ruta, 'r', encoding="utf-8") as file:
+        reader = csv.reader(file)
+        for line in reader:
+            ubicacion = line[1].split(" / ")
         
-        for prov in ubicacion: 
-            if prov in provincias:
-                provincias[prov]["lagos"].append(line[0])
+            for prov in ubicacion: 
+                if prov in provincias:
+                    provincias[prov]["lagos"].append(line[0])
 
 def __agregar_conectividad_a_provincias(datos_conectividad, header, provincias):
     """
@@ -80,15 +69,16 @@ def __agregar_conectividad_a_provincias(datos_conectividad, header, provincias):
         header (list): Una lista que contiene los encabezados de los datos de conectividad.
         provincias (dict): Un diccionario que contiene las provincias a las que se agregarán los datos de conectividad.
     """
-    
-    for line in datos_conectividad:
-        ubicacion = line[0]
+    with open(ruta, 'r', encoding="utf-8") as file:
+        reader = csv.reader(file)
+        for line in datos_conectividad:
+            ubicacion = line[0]
         
-        if ubicacion in provincias:
-            #Desde el 4 al 12 estan los campos de conectividad
-            for i in range(4, 13):
-                if line[i] == "SI" and header[i] not in provincias[ubicacion]["tipos_conectividad"]:
-                    provincias[ubicacion]["tipos_conectividad"].append(header[i])
+            if ubicacion in provincias:
+                #Desde el 4 al 12 estan los campos de conectividad
+                for i in range(4, 13):
+                    if line[i] == "SI" and header[i] not in provincias[ubicacion]["tipos_conectividad"]:
+                        provincias[ubicacion]["tipos_conectividad"].append(header[i])
                     
 def __agregar_aeropuertos_a_provincias(datos_aeropuertos, provincias):
     """
@@ -98,12 +88,14 @@ def __agregar_aeropuertos_a_provincias(datos_aeropuertos, provincias):
         datos_aeropuertos (list): Una lista que contiene los datos de los aeropuertos.
         provincias (dict): Un diccionario que contiene las provincias a las que se agregarán los datos de los aeropuertos.
     """
-    for line in datos_aeropuertos:
+    with open(ruta, 'r', encoding="utf-8") as file:
+        reader = csv.reader(file)
+        for line in datos_aeropuertos:
             #En la columna 23 del dataset custom esta el campo prov_name y en el 3 el nombre del aeropuerto
             if (line[23] in provincias):
                 provincias[line[23]]["aeropuertos"].append(line[3])
 
-def mostrarSegunPoblacion():
+def mostra_segun_poblacion(censo,aeropuertos,lagos,conectividad):
     """
     Muestra información sobre provincias basada en la cantidad de gente proporcionada por el usuario y el signo
     ('>' o '<') para comparar con la población de cada provincia.
@@ -113,13 +105,7 @@ def mostrarSegunPoblacion():
     """
     ecuacion = __pedir_datos_usuario()
     
-    censo = Path('..','datasets') / "c2022_tp_c_resumen_adaptado.csv"
-    aeropuertos = Path('..','datasets_custom') / "ar-airports.csv"
-    lagos = Path('..','datasets') / "lagos_arg.csv"
-    conectividad = Path('..','datasets_custom') / "Conectividad_Internet.csv"
-    
-    datos_censo = __leer_archivo_csv(censo)
-    provincias = __filtrar_provincias_por_poblacion(ecuacion, datos_censo[2:])
+    provincias = __filtrar_provincias_por_poblacion(ecuacion, censo)
     
     datos_aeropuertos = __leer_archivo_csv(aeropuertos)
     __agregar_aeropuertos_a_provincias(datos_aeropuertos, provincias)
