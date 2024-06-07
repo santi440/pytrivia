b9218e7871e6a3b46b3827a33d43bec632cc293e
Bautista Rosli <roslibautista05@gmail.com>
2024-06-07 10:17:54 -0300

    cambios

--- Pytrivia/Pages/01_Datos.py
+++ Pytrivia/Pages/01_Datos.py
@@ -1,3 +1,24 @@
+from FuncionesDatos import graficos as func
 import streamlit as st
+import pandas as pd
+import matplotlib as plt
+from pathlib import Path
 
-st.header("Coming soon...")
+
+st.header("Conocemos nuestros datos")
+st.subheader("Mapa de Aeropuertos")
+
+airports_path = Path("..","datasets_custom")/ 'ar-airports-custom.csv'
+df_airports = pd.read_csv(airports_path)
+
+lakes_path = Path("..","datasets_custom") / "lagos_arg_custom.csv"
+df_lakes = pd.read_csv(lakes_path)
+
+option = st.selectbox("Elija que datos ver", options = ['Aeropuertos','Lagos'])
+
+match option:
+    case 'Aeropuertos':
+        mapa = func.airport_map(df_airports)
+        mapa
+    case 'Lagos':
+        st.write('b')
