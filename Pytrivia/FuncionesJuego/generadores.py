5d33a77748cdf51040de54e0341479670b6b86a9
Ignacio <battaglinoignacio@gmail.com>
2024-06-18 20:45:51 -0300

    Juego.py cambios

--- Pytrivia/FuncionesJuego/generadores.py
+++ Pytrivia/FuncionesJuego/generadores.py
@@ -1,27 +1,173 @@
+import pandas as pd
+import random
+import numpy as np
+from unidecode import unidecode
+
+# Función para verificar puntos
+def check_points(answers, diff):
+    cant = 0
+    points = 0
+    for answer in answers:
+        if unidecode(answer[0].lower().replace(" ", "")) == unidecode(str(answer[1]).lower().replace(" ", "")):
+            cant += 1
+    points = cant
+    if diff == 'Media':
+        points += points * 0.5
+    elif diff == 'Alta':
+        points += points
+    return cant, points
+
+# Función para generar pistas
+def generate_hint(answer, difficulty):
+    numeric_types = (int, float, np.int64, np.longlong)
+    if difficulty == "Fácil":
+        if isinstance(answer, numeric_types):
+            return f"La respuesta está entre {answer - 5} y {answer + 5}"
+        elif isinstance(answer, str) or isinstance(answer, bool):
+            answer_str = str(answer)
+            return f"La respuesta comienza con '{answer_str[0]}' y termina con '{answer_str[-1]}'"
+    elif difficulty == "Media":
+        if isinstance(answer, numeric_types):
+            return f"La respuesta está entre {answer - 10} y {answer + 10}"
+        elif isinstance(answer, str) or isinstance(answer, bool):
+            answer_str = str(answer)
+            return f"La respuesta comienza con '{answer_str[0]}'"
+    return None
+
+# Función para generar preguntas de aeropuertos
 def generar_pregunta_aeropuertos(df):
-    fila = df.sample().iloc[0]
-    opciones = df[df['type'] == fila['type']].sample(3)
-    opciones = opciones.append(fila)
-    opciones = opciones.sample(frac=1)  # Mezclar las opciones
-    return fila['name'], opciones
+    row = df.sample(n=1).iloc[0]
+    question = f"Complete la opcion faltante de los siguientes datos de un aeropuerto\n"
+    
+    # Definir atributos disponibles para completar y mostrar
+    completable = ("Municipio","Nombre","Provincia","Elevacion","Codigo Iata")
+    showable = ("Municipio","Nombre","Provincia","Elevacion","Codigo Iata")
+    
+    # Seleccionar aleatoriamente el atributo a completar y los mostrados
+    complete = random.choice(completable)
+    shown = random.sample(showable, 3)
+    
+    # Asegurar que el atributo a completar no esté entre los mostrados
+    while complete in shown:
+        completar = random.choice (completable)
+    
+    options = {
+        "Municipio": "municipality",
+        "Nombre": "name",
+        "Provincia": "prov_name",
+        "Elevación": "elevation_name",
+        "Código IATA": "iata_code"
+    }
+    
+    for key in shown:
+        question+= f"- {key}: {row[options[key]]}\n"
+    
+    question+= f"- {complete}: ???????"
+        
+    correct_answer = row[options[complete]]
+    
+    return question, correct_answer
 
+# Función para generar preguntas de lagos
 def generar_pregunta_lagos(df):
-    fila = df.sample().iloc[0]
-    opciones = df[df['size'] == fila['size']].sample(3)
-    opciones = opciones.append(fila)
-    opciones = opciones.sample(frac=1)  # Mezclar las opciones
-    return fila['name'], opciones
+    row = df.sample(n=1).iloc[0]
+    question = f"Complete la opcion faltante de los siguientes datos de un lago\n"
+    
+    # Definir atributos disponibles para completar y mostrar
+    completable = ("Nombre","Provincia")
+    showable = ("Nombre","Provincia","Superficie","Profundidad Maxima (en metros)")
+    
+    # Seleccionar aleatoriamente el atributo a completar y los mostrados
+    complete = random.choice(completable)
+    shown = random.sample(showable, 3)
+    
+    # Asegurar que el atributo a completar no esté entre los mostrados
+    while complete in shown:
+        completar = random.choice (completable)
+    
+    options = {
+        "Nombre": "Nombre",
+        "Provincia": "Ubicación",
+        "Superficie (En km²)": "Superficie (km²)",
+        "Profundidad Maxima (en metros)": "Profundidad máxima (m)"
+    }
+    
+    for key in shown:
+        question+= f"- {key}: {row[options[key]]}\n"
 
+    question+= f"- {complete}: ???????"
+    
+    correct_answer = row[options[complete]]
+    
+    return question, correct_answer
+
+# Función para generar preguntas de conectividad
 def generar_pregunta_conectividad(df):
-    fila = df.sample().iloc[0]
-    opciones = df[df['connectivity_type'] == fila['connectivity_type']].sample(3)
-    opciones = opciones.append(fila)
-    opciones = opciones.sample(frac=1)  # Mezclar las opciones
-    return fila['city'], opciones
+    row = df.sample(n=1).iloc[0]
+    question = f"Complete con SI o NO la opcion faltante de la siguiente localidad:\n"
+    
+    # Definir atributos disponibles para completar y mostrar
+    completable = ("Tiene ADSL?", "Tiene CABLEMODEM?", "Tiene DIALUP?", "Tiene FIBRAOPTICA?", 
+               "Tiene SATELITAL?", "Tiene WIRELESS?", "Tiene TELEFONIAFIJA?", "Tiene 3G?", "Tiene 4G?")
 
+    showable = ("Provincia","Partido","Localidad")
+    
+    # Seleccionar aleatoriamente el atributo a completar y los mostrados
+    complete = random.choice(completable)
+    shown = showable
+    
+    options = {
+    "Tiene ADSL?": "ADSL",
+    "Tiene CABLEMODEM?": "CABLEMODEM",
+    "Tiene DIALUP?": "DIALUP",
+    "Tiene FIBRAOPTICA?": "FIBRAOPTICA",
+    "Tiene SATELITAL?": "SATELITAL",
+    "Tiene WIRELESS?": "WIRELESS",
+    "Tiene TELEFONIAFIJA?": "TELEFONIAFIJA",
+    "Tiene 3G?": "3G",
+    "Tiene 4G?": "4G"
+}
+
+    
+    for key in shown:
+        question+= f"- {key}: {row[key]}\n"
+
+    question+= f"- {complete}: ???????"
+    
+    correct_answer = row[options[complete]]
+    
+    return question, correct_answer
+
+# Función para generar preguntas del censo 2022
 def generar_pregunta_censo(df):
-    fila = df.sample().iloc[0]
-    opciones = df[df['population'] == fila['population']].sample(3)
-    opciones = opciones.append(fila)
-    opciones = opciones.sample(frac=1)  # Mezclar las opciones
-    return fila['jurisdiction'], opciones
+    row = df.sample(n=1).iloc[0]
+    question = f"Cual es la provincia que cumple con los siguientes datos?\n"
+    
+    # Definir atributos disponibles para completar y mostrar
+    showable = ("Total de población","Población en situación de calle(²)","Porcentaje de población en situación de calle","Mujeres Total de población","Varones Total de población")
+    
+    # Seleccionar aleatoriamente el atributo a completar y los mostrados
+    complete = "Jurisdicción"
+    shown = random.sample(showable, 3)
+    
+    for key in shown:
+        question+= f"- {key}: {row[key]}\n"
+    
+    question+= f"- Provincia: ???????"
+        
+    correct_answer = row[complete]
+    
+    return question, correct_answer
+
+# Función para generar preguntas basado en el tema
+def generateQuestions(theme, df_aeropuertos, df_lagos, df_conectividad, df_censo):
+    if theme == "Aeropuertos":
+        return [generar_pregunta_aeropuertos(df_aeropuertos) for _ in range(5)]
+    elif theme == "Lagos":
+        return [generar_pregunta_lagos(df_lagos) for _ in range(5)]
+    elif theme == "Conectividad":
+        return [generar_pregunta_conectividad(df_conectividad) for _ in range(5)]
+    elif theme == "Censo 2022":
+        return [generar_pregunta_censo(df_censo) for _ in range(5)]
+    return []
+
--- Pytrivia/pages/02_Juego.py
+++ Pytrivia/pages/02_Juego.py
@@ -1,8 +1,15 @@
 import streamlit as st
 import pandas as pd
+import random
+import time
+from datetime import datetime
 from pathlib import Path
 from FuncionesJuego import generadores as gen
 from FuncionesSesion import Sesiones
+import numpy as np
+
+# Título de la página
+st.title('Juego de Trivia')
 
 # Definir la ruta base
 base_path = Path(__file__).resolve().parent.parent.parent
@@ -20,46 +27,71 @@ conectividad = pd.read_csv(conec_path)
 censo_path = base_path / 'datasets_custom' / 'Censo_Modificado.csv'
 censo_2022 = pd.read_csv(censo_path)
 
-
-# Título de la página
-st.title('Juego de Trivia')
+base_path= Path (__file__).resolve().parent.parent
+datos_path= base_path / 'csv' / 'datos_formularios.csv'
+resultado_path= base_path/ 'csv' / 'resultado.csv'
+datos= pd.read_csv(datos_path)
+    
 if(Sesiones.is_user_logged_in()):
     # Seleccionar usuario
-    usuario = st.selectbox('Selecciona tu usuario', ['Usuario1', 'Usuario2', 'Usuario3'])
+    user = st.selectbox('Selecciona tu usuario', options= datos['Email'].unique())
 
     # Seleccionar temática
-    tematica = st.selectbox('Selecciona una temática', ['Aeropuertos', 'Lagos', 'Conectividad', 'Censo 2022'])
+    theme = st.selectbox('Selecciona una temática', ['Aeropuertos', 'Lagos', 'Conectividad', 'Censo 2022'])
 
     # Seleccionar dificultad
-    dificultad = st.selectbox('Selecciona la dificultad', ['Fácil', 'Media', 'Alta'])
+    difficulty = st.selectbox('Selecciona la dificultad', ['Fácil', 'Media', 'Alta'])
 
-    # Generar preguntas
-    if st.button('Comenzar Juego'):
-        preguntas = []
-        if tematica == 'Aeropuertos':
-            for _ in range(5):
-                preguntas.append(gen.generar_pregunta_aeropuertos(aeropuertos))
-        elif tematica == 'Lagos':
-            for _ in range(5):
-                preguntas.append(gen.generar_pregunta_lagos(lagos))
-        elif tematica == 'Conectividad':
-            for _ in range(5):
-                preguntas.append(gen.generar_pregunta_conectividad(conectividad))
-        elif tematica == 'Censo 2022':
-            for _ in range(5):
-                preguntas.append(gen.generar_pregunta_censo(censo_2022))
+    # Explicacion de como funciona cada nivel de dificultad.
 
-        # Mostrar preguntas
-        for idx, (respuesta_correcta, opciones) in enumerate(preguntas):
-            st.write(f'Pregunta {idx+1}')
-            opcion_seleccionada = st.radio(f'Selecciona una opción:', opciones['name'].tolist())
-            if opcion_seleccionada == respuesta_correcta:
-                st.success('¡Correcto!')
-            else:
-                st.error(f'Incorrecto. La respuesta correcta era: {respuesta_correcta}')
+    st.info("""
+        **Fácil:** Se proporciona una pista detallada.  
+        **Media:** Se proporciona una pista general.  
+        **Alta:** No se proporciona ninguna pista.
+    """)
+    
+    # Generar preguntas basadas en la temática
+    questions = gen.generateQuestions(theme, aeropuertos, lagos, conectividad, censo_2022)
+    
+    # Almacenar respuestas del usuario
+    user_answers = []
+    for i, (question, correct_answer) in enumerate(questions):
+        st.write(f"Pregunta {i + 1}:")
+        st.write(question)
+        
+        if difficulty != 'Alta':
+            hint = gen.generate_hint(correct_answer, difficulty)
+            if hint:
+                st.write(f"Pista: {hint}")
+
+        user_answer = st.text_input(f"Respuesta a la pregunta {i + 1}:")
+        user_answers.append((user_answer, correct_answer))
+
+    # Empezar con las 5 preguntas del tema elegido, dar la pista correspondiente a la dificultad. Si es dificil no hay pista. Sumar 1 punto por cada respuesta correcta.
+
+    # Mostrar mensaje que diga Juego completado y la cantidad de respuestas correctas. Si la dificultad es media el puntaje se multiplica por 1.5, si es dificil el puntaje se multiplica x2.
+
+    # Almacenar en resultado_path : - Fecha y hora: momento en el cual se responde la trivia.
+#                                   - Usuario: identificador del usuario.
+#                                   - Email: email del usuario.
+#                                   - Dificultad: dificultad elegida por el usuario.
+#                                   - Temática: temática elegida por el usuario.
+#                                   - Cantidad de respuestas correctas.
+#                                   - Puntos: puntos obetinos en la partida.
+
+
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        if st.button("Volver a jugar"):
+            st.rerun()
+    
+    with col2:
+        if st.button("Ir al Ranking"):
+            st.switch_page("pages/05_Ranking.py")
+
 
 else:
     st.subheader("Antes de jugar, debes Registrarte o Iniciar Sesion")
     if (st.button('Registrarse')):
         st.switch_page("pages/03_Formulario de Registro.py")
-
