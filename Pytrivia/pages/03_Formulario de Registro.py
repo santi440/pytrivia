a09a0c1f3da57a0a7eb11dcb045fc21dfa3d9a5d
santi <santiago0105@hotmail.com.ar>
2024-06-19 20:30:15 -0300

    nuevo formulario

--- Pytrivia/pages/03_Formulario de Registro.py
+++ Pytrivia/pages/03_Formulario de Registro.py
@@ -1,52 +1,21 @@
 import streamlit as st
-import csv
-import pathlib
-
+import pandas as pd
 from datetime import date
 
-
 def save_form_csv(username, full_name, email, birth_date, gender):
-    path = pathlib.Path('csv/datos_formularios.csv')
-    EMAIL = 2
-    ok = False
-    create_header = False
+    path = 'csv/datos_formularios.csv'
+    df = pd.read_csv(path)
 
-    with path.open(mode='r', encoding='UTF-8') as file:
-        reader = csv.reader(file)
-
-        # chequea si csv esta vacio
-        if path.stat().st_size == 0:
-            create_header = True
-
-        for line in reader:
-            if line[EMAIL] == email:
-                ok = True
-                break
-        
-    if ok:
-        lines = []
-        with path.open(mode='r', encoding='UTF-8') as file:
-            reader = csv.reader(file)
-
-            for line in reader:
-                if line[EMAIL] == email:
-                    line = [username, full_name, email, birth_date, gender]
-                lines.append(line)
-
-        with path.open(mode='w', encoding='UTF-8', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerows(lines)
+    if email in df['Email'].values:
+        df.loc[df['Email'] == email, ['Usuario', 'Nombre completo', 'Email', 'Nacimiento', 'Genero']] = [username, full_name, email, birth_date, gender]
     else:
-        with path.open(mode='a', encoding='UTF-8', newline='') as file:
-            writer = csv.writer(file)
+        # Agregar una nueva fila al DataFrame
+        new_row = {'Usuario': username, 'Nombre completo': full_name, 'Email': email, 'Nacimiento': birth_date, 'Genero': gender}
+        df = df._append(new_row, ignore_index=True)
 
-            if create_header:
-                header = ['Usuario', 'Nombre completo', 'Email', 'Nacimiento','Genero']
-                writer.writerow(header)
+    df.to_csv(path, index=False)
 
-            writer.writerow([username, full_name, email, birth_date, gender])
-
-
+# Configuración de la aplicación Streamlit
 st.title("Formulario de Registro")
 
 # Campos del formulario
@@ -54,21 +23,20 @@ with st.form("my_form"):
     username = st.text_input("Nombre de Usuario")
     full_name = st.text_input("Nombre Completo")
     email = st.text_input("Mail")
-    birth_date = st.date_input("Fecha de Nacimiento", min_value= date(1900,1,1))
+    birth_date = st.date_input("Fecha de Nacimiento", min_value=date(1900, 1, 1))
     gender = st.selectbox("Género", ["Masculino", "Femenino", "Otro"])
     
     # Botón para enviar el formulario
     submitted = st.form_submit_button("Enviar")
 
-    # Chequeo de que el usuario ingrese bien los datos
+    # Validaciones del formulario
     if submitted:
         if not username or not full_name or not email:
-            st.error ("Complete todos los campos para continuar.")
+            st.error("Complete todos los campos para continuar.")
         elif not '@' in email or not '.com' in email:
-            st.error ("Ingrese un email válido.")
+            st.error("Ingrese un email válido.")
         elif birth_date >= date.today():
-            st.error ("Ingrese una fecha válida.")
+            st.error("Ingrese una fecha válida.")
         else:
             save_form_csv(username, full_name, email, birth_date, gender)
             st.success("Formulario enviado con éxito!")
-            
